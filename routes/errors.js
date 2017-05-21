1a8d77fb74c5562e616ae4cc8a1ecbc7951092ee
Brian C. Lindner <brian@bclindner.com>
2017-05-21 06:13:39 -0400

    site now looks s l i c c

--- routes/errors.js
+++ routes/errors.js
@@ -1,6 +1,11 @@
 module.exports = function (app) {
   // 404 error
   app.use((req, res, next) => {
-      res.status(404).render('error', {code: '404'})
+    res.status(404).render('error', {code: '404'})
+  })
+  // 5xx error
+  app.use((err, req, res, next) => {
+    console.log(err.stack)
+    res.status(500).render('error', {code: '500'})
   })
 }
--- routes/static.js
+++ routes/static.js
@@ -2,7 +2,7 @@ module.exports = function (app) {
   app.get('/', (req, res) => {
     res.render('main/index')
   })
-  app.get('/downloads', (req,res) => {
+  app.get('/downloads', (req, res) => {
     res.render('main/downloads')
   })
 }
