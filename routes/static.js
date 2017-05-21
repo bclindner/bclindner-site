0649ddb7fd806e9a0287b766f426f0bc82fb6ea9
Brian C. Lindner <brian@bclindner.com>
2017-05-21 05:34:21 -0400

    downloads page now active

--- routes/static.js
+++ routes/static.js
@@ -2,4 +2,7 @@ module.exports = function (app) {
   app.get('/', (req, res) => {
     res.render('main/index')
   })
+  app.get('/downloads', (req,res) => {
+    res.render('main/downloads')
+  })
 }
